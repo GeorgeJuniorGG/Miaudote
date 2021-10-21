3777f8c67eb2ec9a7c341a49828ef75d2ac87f8b
GeorgeJuniorGG <g216741@dac.unicamp.br>
2021-10-21 14:58:41 -0300

    Corrigindo posicionamentos e adicionando PopUps e Termos de Uso

--- app/libs/baseclass/SignUpScreen.py
+++ app/libs/baseclass/SignUpScreen.py
@@ -1,7 +1,14 @@
 from kivymd.uix.floatlayout import MDFloatLayout
 from kivymd.uix.screen import MDScreen
-from kivymd.uix.button import MDFlatButton
 from kivymd.uix.dialog import MDDialog
 
 class SignUpScreen(MDFloatLayout, MDScreen):
-    pass
+    dialog = None
+
+    def show_protectors_dialog(self):
+        if not self.dialog:
+            self.dialog = MDDialog(
+                title="Protetores",
+                text="Protetores são pessoas com bom coração que cuidam de animais até eles encotrarem uma lar.\n\nMarque essa opção se você tem interesse em divulgar animais para serem adotados.",
+            )
+        self.dialog.open()
