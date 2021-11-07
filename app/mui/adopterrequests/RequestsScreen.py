08b288975966fd4a43338832440f916889a5d444
GeorgeJuniorGG <g216741@dac.unicamp.br>
2021-11-07 16:16:28 -0300

    Adicionando telas: Requests, RequestsReceived e Favorites

--- app/mui/adopterrequests/RequestsScreen.py
+++ app/mui/adopterrequests/RequestsScreen.py
@@ -1,5 +1,29 @@
-from kivymd.uix.floatlayout import MDFloatLayout
+from logging import root
 from kivymd.uix.screen import MDScreen
+from kivymd.uix.floatlayout import MDFloatLayout
+from kivymd.uix.list.list import IconRightWidget, ImageLeftWidget, ThreeLineAvatarIconListItem
+from kivy.clock import Clock
 
-class RequestsScreen(MDFloatLayout, MDScreen):
-    pass
+from mui.ColorTheme import Color
+
+class RequestsScreen(MDScreen, MDFloatLayout):
+    def __init__(self, **kw):
+        super().__init__(**kw)
+        Clock.schedule_once(self.on_start)
+    
+    def on_start(self, interval):
+        cor = Color()
+
+        for i in range(9):
+            item = ThreeLineAvatarIconListItem(text=f"Mr. Bubbles {i}", 
+                   theme_text_color="Custom", text_color=cor.rgbVermelho(),
+                   secondary_text="Características", tertiary_text = "Bubbles...",
+                   tertiary_theme_text_color="Custom", tertiary_text_color=cor.textoL(), 
+                   divider_color=cor.rgbVermelho())
+            image = ImageLeftWidget(source = "mrbubbles.png")
+            icon = IconRightWidget(icon = 'close', theme_text_color = "Custom", text_color= cor.detalheClaro())
+
+            item.add_widget(image)
+            item.add_widget(icon)
+            self.ids.container.add_widget(item)
+
--- app/mui/protectorrequests/RequestsReceivedScreen.py
+++ app/mui/protectorrequests/RequestsReceivedScreen.py
@@ -1,5 +1,25 @@
-from kivymd.uix.floatlayout import MDFloatLayout
+from logging import root
 from kivymd.uix.screen import MDScreen
+from kivymd.uix.floatlayout import MDFloatLayout
+from kivymd.uix.list.list import IconLeftWidget, OneLineAvatarListItem
+from kivy.clock import Clock
 
-class RequestsReceivedScreen(MDFloatLayout, MDScreen):
-    pass
+from mui.ColorTheme import Color
+
+class RequestsReceivedScreen(MDScreen, MDFloatLayout):
+    def __init__(self, **kw):
+        super().__init__(**kw)
+        Clock.schedule_once(self.on_start)
+    
+    def on_start(self, interval):
+        cor = Color()
+
+        for i in range(9):
+            item = OneLineAvatarListItem(text=f"Usuário {i}", 
+                   theme_text_color="Custom", text_color=cor.rgbVermelho(),
+                   divider_color=cor.rgbVermelho())
+            icon = IconLeftWidget(icon = "account-circle")
+            
+            item.add_widget(icon)
+            self.ids.container.add_widget(item)
+
