22e5e161260cc741ab7dc26a89e301c5e209eb93
Jhonatan Cléto <j256444@dac.unicamp.br>
2021-11-14 22:17:52 -0300

    Novas Telas e Ligação entre telas

--- app/mui/home/HomeScreen.py
+++ app/mui/home/HomeScreen.py
@@ -1,14 +1,13 @@
 from logging import root
 from kivymd.uix.screen import MDScreen
 from kivymd.uix.floatlayout import MDFloatLayout
-from kivymd.uix.list.list import IRightBody, ThreeLineAvatarListItem, ImageLeftWidget
 from kivy.clock import Clock
 
 from mui.ColorTheme import Color
 from .components.PetItem import PetItem
 from .components.Separator import Separator
 
-class HomeScreen(MDScreen, MDFloatLayout):
+class HomeScreen(MDScreen):
     def __init__(self, **kw):
         super().__init__(**kw)
         # Apenas para ilustrar
@@ -24,7 +23,6 @@ class HomeScreen(MDScreen, MDFloatLayout):
         for i in range(9):
             items.append(item)
 
-        #Clock.schedule_once(self.on_start)
         Clock.schedule_once(lambda x: self.insert_items(items))
     
     def insert_items(self, items:list):
@@ -36,20 +34,6 @@ class HomeScreen(MDScreen, MDFloatLayout):
             self.ids.container.add_widget(Separator())
             self.ids.container.ids[f'item{i}'] = petItem
 
-    def on_start(self, interval):
-        cor = Color()
-
-        for i in range(9):
-            item = ThreeLineAvatarListItem(text=f"Mr. Bubbles {i}", 
-                   theme_text_color="Custom", text_color=cor.rgbVermelho(),
-                   secondary_text="Características", tertiary_text = "Bubbles...",
-                   tertiary_theme_text_color="Custom", tertiary_text_color=cor.textoL(), 
-                   divider_color=cor.rgbVermelho())
-            image = ImageLeftWidget(source = "mrbubbles.png")
-
-            item.add_widget(image)
-            self.ids.container.add_widget(item)
-    
     def search(self, search_text):
         print("BUSCAR --> " + search_text)
 
---
+++ app/mui/root/RootScreen.py
@@ -0,0 +1,48 @@
+from kivy.uix.behaviors.button import ButtonBehavior
+from kivy.uix.screenmanager import ScreenManager, SlideTransition
+from kivymd.uix.behaviors.elevation import FakeRectangularElevationBehavior
+from kivymd.uix.screen import MDScreen
+from kivymd.uix.floatlayout import MDFloatLayout
+from kivymd.uix.gridlayout import MDGridLayout
+from kivy.properties import (
+    StringProperty,
+    ObjectProperty
+)
+
+class MiaudoteLogo(MDFloatLayout):
+    ...
+
+class MiaudoteNavBar(FakeRectangularElevationBehavior, MDFloatLayout):
+    tabManager = ObjectProperty(ScreenManager)
+    buttonContainer = ObjectProperty(MDGridLayout)
+
+    def home(self):
+        homeItem = self.ids.home_icon
+        homeItem.ids.icon.text_color = '#5D5FEF'
+        homeItem.ids.label.text_color = '#5D5FEF'       
+
+    def changeTab(self, tabName:str, transition):
+        self.tabManager.transition = SlideTransition()
+        self.tabManager.transition.direction = transition
+        self.tabManager.current = tabName
+
+    def changeButtonColor(self, selItem):
+        for itemName in self.ids:
+            item = self.ids[itemName]
+            if item != self.buttonContainer:
+                if item != selItem:
+                    item.ids.label.text_color = '#FFFFFF'
+                    item.ids.icon.text_color = '#FFFFFF'
+        
+        selItem.ids.icon.text_color = '#5D5FEF'
+        selItem.ids.label.text_color = '#5D5FEF'
+
+class NavBarItem(ButtonBehavior, MDFloatLayout):
+    icon = StringProperty()
+    text = StringProperty()
+
+class ManagerRootTabs(ScreenManager):
+    tabPanel = ObjectProperty(MiaudoteNavBar)
+
+class RootScreen(MDScreen):
+    ...
