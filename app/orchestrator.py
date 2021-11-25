9835ad94e6a20a1ea90a3e57e0f7a6acb1e31814
Jhonatan Cléto <j256444@dac.unicamp.br>
2021-11-24 22:53:24 -0300

    Melhoria e novas features ochestrator #39.

--- app/orchestrator.py
+++ app/orchestrator.py
@@ -1,97 +1,9 @@
 import json
-from datetime import datetime, time, timedelta
+from datetime import datetime, timedelta
 
 from mem.screenmanager.MainScreenManager import MainScreenManager
-from mem.login.LoginManager import LoginManager
-from mem.signUp.signUpManager import SignUpManager
-from mem.welcome.WelcomeManager import WelcomeManager
-from mem.root.RootManager import RootManager
-
-from mlo.storage.firebaseDB import FirebaseDB
-from mlo.auth.dataModel import DataModel
-from mlo.auth.firebaseAuth import FireBaseAuthService
-from mlo.user.UserService import UserService
-from mlo.database.firebaseUserDB import FUserDB
-from mlo.database.firebasePetDB import FPetDB
-from mlo.pets.PetService import PetService
-
 from mem.screenmanager.screens import screens
-
-
-screenRecipes = {
-
-    screens['welcome']: {
-        'db': {
-            'class': None,
-            'args': None
-        },
-        'service': {
-            'class': None,
-            'args': None            
-        },
-        'controller': {
-            'class': WelcomeManager,
-            'args': None
-        },
-        
-    },
-
-    screens['login']: {
-        'db': {
-            'class': None,
-            'args': None
-        },
-        'service': {
-            'class': FireBaseAuthService,
-            'args': None            
-        },
-        'controller': {
-            'class': LoginManager,
-            'args': None
-        },    
-    },
-
-    screens['signUp']: {
-        'db': {
-            'class': None,
-            'args': None
-        },
-        'service': {
-            'class': FireBaseAuthService,
-            'args': None            
-        },
-        'controller': {
-            'class': SignUpManager,
-            'args': None
-        },
-    },
-
-    screens['root']: {
-        'db': {
-            'class': FUserDB,
-            'args': ('userID',)
-        },
-        'service': {
-            'class': UserService,
-            'args': None            
-        },
-        'controller': {
-            'class': RootManager,
-            'args': ('orchestrator',)
-        },
-    },
-    screens['home']: {
-        'db': {
-            'class': FPetDB,
-            'args': None
-        },
-        'service': {
-            'class': PetService,
-            'args': None            
-        }        
-    }
-}
-
+from recipeBook import recipes
 
 class Orchestrator:
 
@@ -100,15 +12,17 @@ class Orchestrator:
     def __init__(self, manager:MainScreenManager) -> None:
         self.manager = manager
         self.manager.orchestrator = self
+        self.__components = dict()
         self.userID = self.__openUserFile()
     
+    # Abrir o arquivo com as informações de login do Usuário
+    # Permite mudança de flow no app
     def __openUserFile(self):
         with open(self.__USER_DATA, 'r+', encoding='utf-8') as uFile:
             suData = uFile.read()
             if suData == "":
                 return ""
             uData = json.loads(suData)
-
             lastLogin = datetime.strptime(uData['lastLogin'], '%Y-%m-%d %H:%M:%S.%f')
             now = datetime.now()
             interv = lastLogin + timedelta(days=8)
@@ -117,114 +31,101 @@ class Orchestrator:
 
             return uData['userID']
 
+    # Determina qual deve ser o flow ao iniciar o app
     def appFlow(self):
         appFlow = self.__welcomeFlow
+        print(self.userID)
         if self.userID != "":
             appFlow = self.__rootFlow
 
         appFlow()
 
+    # Coloca algumas propriedades do objeto em um dicionário
+    # necessária para obter alguns argumentos de componentes
     def dict(self):
         return {
             'userID': self.userID,
             'orchestrator': self
         }
 
-    def __makeComponent(self, comp:dict, fArg=None, outArgs={}):
-        if comp['args'] == None:
-            if fArg == None:
-                return comp['class']()
+    # Adiciona uma referência de um componente ao orchestrator
+    # minimiza a necessiade de recursão em __buildComponent
+    # faz com que componentes que usam a mesma dependência a compartilhem
+    def __addComponent(self, cName:str, component):
+        self.__components[cName] = component
+    
+    # Carrega os argumentos primitivos requeridos por um componente
+    def __loadArgs(self, cArgs, outArgs:dict):
+        pArgs = list()
+        if cArgs != None:
+            inArgs = self.dict()
+            inArgsKeys = inArgs.keys()
+            outArgsKeys = outArgs.keys()
+            for arg in cArgs:
+                if arg in inArgsKeys:
+                    pArgs.append(inArgs[arg])
+                elif arg in outArgsKeys:
+                    pArgs.append(outArgs[arg])
 
-            return comp['class'](fArg)
+        return pArgs
 
-        # TODO
-        # carregar os argumentos do Componente usando dados do Orquestrador
-        inArgs = self.dict()
-        #print(inArgs)
-        args = list()
-        for arg in comp['args']:
-            #print(arg)
-            if arg in inArgs.keys():
-                args.append(inArgs[arg])
-            elif arg in outArgs.keys():
-                args.append(outArgs[arg])
+    # Verifica se uma dependência já foi criada
+    # TODO?: Futuramente talvez seja necessário estabelecer
+    #       uma indicação de exclusividade para alguns componentes
+    #       que não podem ser compartilhados entre diferentes telas
+    def __checkDep(self, dep):
+        if dep in self.__components.keys():
+            return self.__components[dep]
 
-        if fArg != None:
-            return comp['class'](fArg, *args)
-
-        #print(args)
-        return comp['class'](*args)
-
-
-    def __mixComponents(self, comp1:dict, comp2:dict, oA1:dict, oA2:dict):
-        fArg = None
-        if comp1['class'] == None:
-            return None
-
-        if comp2['class'] != None:    
-            fArg = self.__makeComponent(comp2, outArgs=oA2)
-        #print(fArg)
-        
-        return self.__makeComponent(comp1, fArg, outArgs=oA1)
-
-    def __splitArgs(self, outArgs:dict):
-        argList = []
-        for key in ['db', 'service', 'controller']:
-            if key in outArgs.keys():
-                argList.append(outArgs[key])
-            else:
-                argList.append({})
-        
-        return argList
-
-    def __startScreen(self, screenName, outArgs:dict={}):
-        screenRecipe = screenRecipes[screenName]
-        dbComponent = screenRecipe['db']
-        serviceComponent = screenRecipe['service']
-        controllerComponent = screenRecipe['controller']
-
-        dbArgs, sArgs, cArgs = self.__splitArgs(outArgs)
-
-        service = self.__mixComponents(serviceComponent, dbComponent, sArgs, dbArgs)
-        controller = self.__makeComponent(controllerComponent, service, cArgs)
-
-        return controller.screen
-
-    def __startSignUp(self):
-        dataModel = DataModel()
-        auth = FireBaseAuthService()
-        storege = FirebaseDB()        
-        controller = SignUpManager(auth, dataModel, storege)
-        controller.manager = self.manager
-        controller.addScreens()
         return None
 
-    # Instancia uma Screen do App, inializando todas as suas dependencias
+    # Função recursiva que constroi algum componente do App
+    # instânciando se necessário suas dependências
+    def __buildComponent(self, component:dict, outArgs:dict={}):
+        deps = list()
+        cDeps = component['deps']
+        if cDeps != None:
+            for dep in cDeps:
+                cDep = self.__checkDep(dep)
+                if cDep == None:
+                    cDep = self.__buildComponent(recipes[dep], outArgs)
+                    self.__addComponent(dep, cDep)
+                deps.append(cDep)
+
+        cArgs = component['pArgs']
+        pArgs = self.__loadArgs(cArgs, outArgs)
+        return component['class'](*deps, *pArgs)
+
+    # Constroi um componente do app a partir de sua receita
+    def buildComponent(self, cName, outArgs:dict={}):
+        recipe = recipes[cName]
+        return self.__buildComponent(recipe, outArgs)
+
+    # Inicializa os componentes necessário para criar uma tela do app
     def startScreen(self, screenName, outArgs:dict={}):
+        controller = self.buildComponent(screenName, outArgs)
+        # O signUpManager Possui 4 telas,
+        # por isso optei por fazer a adição de tela dele diferente
         if screenName == screens['signUp']:
-            return self.__startSignUp()
+            controller.manager = self.manager
+            controller.addScreens()
+            return None
+        # retorna a tela para ser adicionada ao ScreenManager
+        return controller.screen
 
-        return self.__startScreen(screenName, outArgs)
-
-    def makeComponent(self, screenName, outArgs:dict={}):
-        compRecipe = screenRecipes[screenName]
-        dbComponent = compRecipe['db']
-        serviceComponent = compRecipe['service']        
-
-        dbArgs, sArgs, dummy = self.__splitArgs(outArgs)
-        service = self.__mixComponents(serviceComponent, dbComponent, sArgs, dbArgs)
-        return service
-
+    # Flow do app relacionado a etapa de login e cadastro de usuários
     def __welcomeFlow(self):
-        welcomeScreen = self.startScreen(screens['welcome'])
-        self.manager.add_widget(welcomeScreen)
+        welcomeManager = self.buildComponent(screens['welcome'])
+        self.manager.add_widget(welcomeManager.screen)
         #self.__startSignUp()
 
-
+    # Flow do app relacionado ao pós-login
     def __rootFlow(self):
-        rootScreen = self.startScreen(screens['root'])
-        self.manager.add_widget(rootScreen)
+        rootManager = self.buildComponent(screens['root'])
+        self.manager.add_widget(rootManager.screen)
 
+    # Armazena as informações de login do Usuário
+    # Permite que o usuário não precise logar novamente
     def __registerLogin(self, userID:str):
         self.userID = userID
         userData = {
@@ -234,9 +135,29 @@ class Orchestrator:
         with open(self.__USER_DATA, 'w', encoding='utf-8') as uFile:
             json.dump(userData, uFile, indent=2)
 
+    # Apaga o registro de login do usuário
+    # util para o recurso de logout
+    def __eraseLoginData(self):
+        with open(self.__USER_DATA, 'w', encoding='utf-8') as uFile:
+            uFile.write("")
+
+        # limpar os dados após o logout
+        self.userID = ""
+        self.__components = dict()
+
+    # Quando o usuário efetua o login essa função salvas as informações de login
+    # e redireciona para a HomeScreen do app
     def userLogin(self, userID:str):
         self.__registerLogin(userID)
         self.manager.clear_widgets()
         self.__rootFlow()
         self.manager.changeScreen('left', screens['root'])
+
+    # Quando o usuário efetuar o logout do app seus dados são apagados
+    # e o app redireciona para a tela de welcome
+    def userLogout(self):
+        self.__eraseLoginData()
+        self.manager.clear_widgets()
+        self.__welcomeFlow()
+        self.manager.changeScreen('left', screens['welcome'])
     
