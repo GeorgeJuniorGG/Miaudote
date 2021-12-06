694486e2f089cd347d84771aef34dc48aed64206
Jhonatan Cléto <j256444@dac.unicamp.br>
2021-12-05 23:02:20 -0300

    atualizar FileManager

--- app/mem/filechooser/FileManager.py
+++ app/mem/filechooser/FileManager.py
@@ -28,7 +28,6 @@ class FileManager:
             self.__openScreen()
 
     def exitScreen(self):
-        print('Passou aqui')
         if self.screen != None:
             self.manager = self.screen.manager
             self.screen = None
